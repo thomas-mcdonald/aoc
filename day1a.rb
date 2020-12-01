86e44e1ecf5f7fe1335d472c8865ac0fef90e723
Thomas McDonald <thomasm@palantir.com>
2020-12-01 13:43:34 +0000

    refactor

--- day1a.rb
+++ day1a.rb
@@ -3,9 +3,9 @@ input = File.read("./day1.input").split("\n").map(&:to_i)
 arr = input.product(input)
 
 arr.each do |x, y|
-  if x + y == 2020
-    puts "#{x} + #{y}"
-    puts x * y
-    break
-  end
+  next unless x + y == 2020
+
+  puts "#{x} + #{y}"
+  puts x * y
+  break
 end
--- day1b.rb
+++ day1b.rb
@@ -3,9 +3,9 @@ input = File.read("./day1.input").split("\n").map(&:to_i)
 arr = input.product(input, input)
 
 arr.each do |x, y, z|
-  if x + y + z == 2020
-    puts "#{x} + #{y} + #{z}"
-    puts x * y * z
-    break
-  end
+  next unless x + y + z == 2020
+
+  puts "#{x} + #{y} + #{z}"
+  puts x * y * z
+  break
 end
