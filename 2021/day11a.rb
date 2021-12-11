79b38346640b6675670466572f6c1fc751463880
Thomas McDonald <tom@conceptcoding.co.uk>
2021-12-11 11:29:43 +0000

    add day 11

---
+++ 2021/day11a.rb
@@ -0,0 +1,47 @@
+require "set"
+
+input = File.read("./day11.input").split("\n").map { |line| line.split("").map(&:to_i) }
+
+board = input
+ticks = 0
+flashes = 0
+
+while ticks < 100
+  ticks += 1
+  new_board = board.dup
+
+  new_board = new_board.map do |line|
+    line.map do |num|
+      num + 1
+    end
+  end
+
+  flashed = Set.new
+
+  while new_board.each_with_index.any? { |line, x| line.each_with_index.any? { |num, y| num > 9 and !flashed.include?([x, y]) } }
+    new_board.each_with_index do |line, x|
+      line.each_with_index do |num, y|
+        next unless num > 9 and !flashed.include?([x, y])
+
+        flashed << [x, y]
+        new_board[x - 1][y - 1] += 1 if x - 1 >= 0 && y - 1 >= 0
+        new_board[x][y - 1] += 1 if y - 1 >= 0
+        new_board[x + 1][y - 1] += 1 if x + 1 < new_board.length && y - 1 >= 0
+        new_board[x - 1][y] += 1 if x - 1 >= 0
+        new_board[x + 1][y] += 1 if x + 1 < new_board.length
+        new_board[x - 1][y + 1] += 1 if x - 1 >= 0 && y + 1 < new_board[0].length
+        new_board[x][y + 1] += 1 if y + 1 < new_board[0].length
+        new_board[x + 1][y + 1] += 1 if x + 1 < new_board.length && y + 1 < new_board[0].length
+      end
+    end
+  end
+
+  flashed.each do |point|
+    new_board[point[0]][point[1]] = 0
+  end
+  flashes += flashed.length
+
+  board = new_board
+end
+
+puts flashes
